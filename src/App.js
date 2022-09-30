977754f79f238943f6d7442076a5b1b55d004339
Kenny Nnakwue <nnakwuekenny@gmail.com>
2022-09-30 15:32:44 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
 
 function App() {
   return (
-    <div className='green-500'>Hey</div>
+    <div className='text-green-500'>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+    </div>
   );
 }
 
