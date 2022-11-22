d3425729225e553a0927d53b2a61678c2e727d87
Kenny Nnakwue <nnakwuekenny@gmail.com>
2022-11-22 13:16:24 +0100

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,17 +10,17 @@ const Contact = () => {
             <form action="#" class="space-y-6 md:border shadow p-3 md:p-8 lg:p-10 rounded-lg w-full max-w-3xl">
                 <div>
                     <label for="email" class="block mb-2 text-[#0a192f] font-semibold italic text-normal">Your email</label>
-                    <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@emailprovider.com" required />
+                    <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:shadow-sm-light" placeholder="name@emailprovider.com" required />
                 </div>
                 <div>
                     <label for="subject" class="block mb-2 text-[#0a192f] font-semibold italic text-normal">Subject</label>
-                    <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let me know how I can help you..." required />
+                    <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:shadow-sm-light" placeholder="Let me know how I can help you..." required />
                 </div>
                 <div class="sm:col-span-2">
                     <label for="message" class="block mb-2 text-[#0a192f] font-semibold italic text-normal">Your message</label>
-                    <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave me a comment..."></textarea>
+                    <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Leave me a comment..."></textarea>
                 </div>
-                <button type="submit" class="py-3 px-5 text-normal font-semibold capitalize text-center text-white rounded-lg bg-green-500 sm:w-fit hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+                <button type="submit" class="py-3 px-5 text-normal font-semibold capitalize text-center text-white rounded-lg bg-green-500 sm:w-fit hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-primary-300">Send message</button>
             </form>
         </div>
       </section>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import WorkImg from '../assets/workImg.jpeg';
 import realEstate from '../assets/realestate.jpg';
+import taskManager from '../assets/task-manager.png';
+import imagoQuad from '../assets/imago-quad.png';
+import sabiVoters from '../assets/sabivoters.png';
 
 const Work = () => {
   return (
     <div name='work' className='flex flex-col w-full bg-[#0a192f] dark:bg-gray-100 flex justify-center items-center p-4 md:py-10 lg:py-12 shadow-xl'>
-      <section class="text-gray-400 max-w-5xl body-font md:px-6">
+      <section class="text-gray-400 max-w-7xl body-font md:px-6">
         <div className='pb-8'>
           <h2 class="flex justify-center mb-8">
             <span className='inline-flex text-4xl tracking-tight font-extrabold font-mono text-center text-gray-300 dark:text-gray-200 border-b-4 border-green-500'>My Projects</span>
@@ -18,7 +21,7 @@ const Work = () => {
           <div class="flex flex-wrap -m-4">
             <div class="lg:w-1/3 sm:w-1/2 p-4">
               <div class="flex relative rounded-xl overflow-hidden">
-                <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="../assets/imago-quad.png" />
+                <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src={imagoQuad} />
                 <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-[90%] text-center">
                   <h2 class="tracking-widest text-sm title-font font-semibold text-green-500 mb-1">iMAGO qUAD</h2>
                   <p class="leading-relaxed">In attempt to clone the features of the android app "4 pics one word", I built this using ReactJS and Tailwindcss.</p>
@@ -31,12 +34,12 @@ const Work = () => {
             </div>
             <div class="lg:w-1/3 sm:w-1/2 h-full p-4">
               <div class="flex relative rounded-xl overflow-hidden">
-                <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="../assets/task-manager.png" />
+                <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src={taskManager} />
                 <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-[90%] text-center">
-                  <h2 class="tracking-widest text-sm title-font font-semibold text-cyan-500 mb-1">tASK mANAGER</h2>
+                  <h2 class="tracking-widest text-sm title-font font-semibold text-cyan-500 mb-1">Taskify</h2>
                   <p class="leading-relaxed">I implemented this using the MERN stack, and designed the UI with tailwindcss CSS utility-first framework. Though, still a work in progress.</p>
                   <div className='w-full flex justify-around py-2'>
-                    <a className='py-1 px-2 border-2 border-cyan-500 rounded text-cyan-500 hover:text-gray-50 hover:bg-cyan-500' href='https://hermit-task-manager.vercel.app'>Visit Site</a>
+                    <a className='py-1 px-2 border-2 border-cyan-500 rounded text-cyan-500 hover:text-gray-50 hover:bg-cyan-500' href='https://taskify-snowy.vercel.app'>Visit Site</a>
                     <a className='py-1 px-2 border-2 border-gray-500 rounded text-gray-500 hover:text-gray-50 hover:bg-gray-500' href='https://github.com/NnakwueKenny/tASK_mANAGER'>Github</a>
                   </div>
                 </div>
@@ -44,7 +47,7 @@ const Work = () => {
             </div>
             <div class="lg:w-1/3 sm:w-1/2 p-4">
               <div class="flex relative rounded-xl overflow-hidden">
-                <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="../assets/sabivoters.png" />
+                <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src={sabiVoters} />
                 <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-[90%] text-center">
                   <h2 class="tracking-widest text-sm title-font font-semibold text-green-700 mb-1">sABI vOTERS</h2>
                   <p class="leading-relaxed">In collaboration with some other PHP developers, I designed the UI of this site using JS and TailwindCss.</p>
@@ -54,7 +57,8 @@ const Work = () => {
                 </div>
               </div>
             </div>
-            <div class="lg:w-1/3 sm:w-1/2 p-4">
+            {/*
+              <div class="lg:w-1/3 sm:w-1/2 p-4">
               <div class="flex relative rounded-xl overflow-hidden">
                 <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="../assets/anjima.png" />
                 <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-[90%] text-center">
@@ -65,7 +69,8 @@ const Work = () => {
                   </div>
                 </div>
               </div>
-            </div>
+              </div>
+            */}
           </div>
         </div>
       </section>
